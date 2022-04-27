02d65801ba72840c28266b6af6fd3c6cc47ddff5
Nick Story <storynickolasl@gmail.com>
2022-04-27 07:10:07 -0400

    commit tree cb1b88f8104002c39014f00c9298d6a9bf49a703

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let result;
+  if(someNumber <= 400) {
+    result = 'This one is on me!'
+  }
+  else if(someNumber > 400 && someNumber <= 2000) {
+    result = 'That will be twenty bucks.'
+  }
+  else if(someNumber > 2000 && someNumber <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    result = 'No can do.'
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response
+  city == 'NYC' ? response = 'Ok, sounds good.' : response = 'No go.';
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response
+  switch(tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;    
+  }
+  return response
 }
